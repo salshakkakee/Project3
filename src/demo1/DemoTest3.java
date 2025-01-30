60ea52026f10e1ffdaa4d5d814d2f49eb2eb5755
salsh <salsh@Sally.lan>
2025-01-29 21:08:46 -0500

    adding grade and salary

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -3,6 +3,11 @@ package demo1;
 public class DemoTest3 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		char grade = 'A';
+		double salary = 22.22;
+		
+		System.out.println(grade + salary);
 	}
 
 }
